76b79bd92cabc342e628e7bc97bb21d509ccbeb2
Soumya795 <soumyasubhakanta97@gmail.com>
2021-10-08 15:52:29 +0530

    IntegerMax

--- src/FindMax.java
+++ src/FindMax.java
@@ -1,28 +1,19 @@
 import java.util.Arrays;
 
-    public class FindMax<F extends Comparable<F>> {
+public class FindMax {
 
-        F[] elements;
-
-        public FindMax(F[] elements) {
-            this.elements = elements;
-        }
-
-        public static <T extends Comparable<T>> T maxOfValues(T[] elements) {
-            Arrays.sort(elements);
-            int length = elements.length;
-            T max = elements[length - 1];
-            System.out.printf("Maximum value of three is %s ", max);
-            return max;
-        }
-
-        public static void main(String[] args) {
-            System.out.println("Welcome to the program to find maximum value using generics");
-            Integer[] intMax = {5, 10, 48, 50, 42, 45, 412, 598};
-            maxOfValues(intMax);
-            Float[] floatMax = {8.35f, 97.8f, 4.45f, 0.8f, 8.6f};
-            maxOfValues(floatMax);
-            String[] stringMax = {"ABC", "IJK", "PQWR", "BCSD", "MNPO", "LTMN", "ZRST", "UXYZ"};
-            maxOfValues(stringMax);
-        }
+    public static Integer maxOfInteger(Integer A, Integer B, Integer C) {
+        Integer max = A;
+        if (B.compareTo(max) > 0)
+            max = B;
+        if (C.compareTo(max) > 0)
+            max = C;
+        return max;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program to find maximum value using generics");
+        Integer A = 50, B = 58, C = 512;
+        System.out.println("The Maximum between three integers is : " + maxOfInteger(A, B, C));
+    }
+}
